6df30b3a6f68a12e3ef6da84db84927863526dc8
Amirxon <shokirovamir2006@gmail.com>
2023-08-12 08:00:26 +0500

    b

--- src/pages/home/components/HomeCategory.jsx
+++ src/pages/home/components/HomeCategory.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { useFetch } from '../../../services/api';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useFetch } from "../../../services/api";
+import item1 from "../../../assets/img/item1.png";
+import item2 from "../../../assets/img/item2.png";
+import item3 from "../../../assets/img/item3.png";
+import item4 from "../../../assets/img/item4.png";
 
 function HomeCategory() {
-  const { data, isPending, error } = useFetch('https://travelagencylanding.pythonanywhere.com/services/');
-  const { i18n, t } = useTranslation(['home']);
+  const { data, isPending, error } = useFetch(
+    "https://travelagencylanding.pythonanywhere.com/services/"
+  );
+  const { i18n, t } = useTranslation(["home"]);
 
   // Store the image URLs in a single array
-  const icons = [
-    './src/assets/img/item1.png',
-    './src/assets/img/item2.png',
-    './src/assets/img/item3.png',
-    './src/assets/img/item4.png',
-  ];
+  const icons = [item1, item2, item3, item4];
 
   return (
     <div className="home_category">
@@ -24,16 +25,16 @@ function HomeCategory() {
         <div className="home_category_cards_details">
           <img src="../../../assets/img/h3.png" alt="" />
         </div>
-        {data && data.map((item, index) => {
-          return (
-            <div key={item.id} className='home_category_card'>
-              {/* Use the icons array with the corresponding index */}
-              <img src={icons[index]} alt='' />
-              <h1>{item[`name_${i18n.language}`]}</h1>
-              <p>{item[`about_${i18n.language}`]}</p>
-            </div>
-          )
-        })}
+        {data &&
+          data.map((item, index) => {
+            return (
+              <div key={item.id} className="home_category_card">
+                <img src={icons[index]} alt="" />
+                <h1>{item[`name_${i18n.language}`]}</h1>
+                <p>{item[`about_${i18n.language}`]}</p>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
